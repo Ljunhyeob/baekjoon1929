052fc11060729a60de5ac8c0684c21035119b313
이준협 <121070166+Ljunhyeob@users.noreply.github.com>
2023-01-07 22:10:08 +0900

    Add files via upload

---
+++ baekjoon1929/baekjoon1929/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  baekjoon1929
+//
+//  Created by 이준협 on 2023/01/07.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+
+let M = input[0]
+let N = input[1]
+
+var arr: [Int] = Array(repeating: 0, count: N + 1)
+for i in 2...N {
+    arr[i] = i
+}
+
+for j in 2...N {
+    if arr[j] == 0 { continue }
+    for k in stride(from: j + j, through: N, by: j) {
+        arr[k] = 0
+    }
+}
+
+for w in M...N {
+    if arr[w] != 0 {
+        print("\(arr[w])")
+    }
+}
